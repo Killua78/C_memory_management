34a79fb5dab5a0828d120eeac0c3b9520778e2ce
Killua78 <nacimeboubekeur@gmail.com>
2026-03-23 19:19:04 +0100

    ff

--- ex4/ft_convert_base.c
+++ ex4/ft_convert_base.c
@@ -6,8 +6,81 @@
 /*   By: nboubeke <nboubeke@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 16:06:29 by nboubeke          #+#    #+#             */
-/*   Updated: 2026/03/21 16:06:31 by nboubeke         ###   ########.fr       */
+/*   Updated: 2026/03/23 18:40:53 by nboubeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	base_len(char *base)
+{
+	int	i;
 
+	i = 0;
+	while (base[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	is_valid_base(char *base)
+{
+	int	i;
+
+	i = 0;
+	if (base_len(base) < 2)
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == base[i + 1] || base[i] == '\n')
+			return (0);
+		if (base[i] == '-' || base[i] == '+')
+			return (0);
+		if (base[i] == ' ' || base[i] == '\t')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	index_in_base(char c, char *base)
+{
+	int	i;
+	
+	i = 0;
+	while (base[i])
+	{
+		if (c == base[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	atoi_base(char *str, char *base)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] && str[i] == '-')
+			count++;
+		i++;
+	}	
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
+			i++;
+		else
+			j = j * base_len(base) + index_in_base(str[i], base);
+		i++;
+	}
+	if (count % 2 == 0)
+		return (j);
+	else
+		return (-j);
+}
--- ex4/ft_convert_base2.c
+++ ex4/ft_convert_base2.c
@@ -6,8 +6,63 @@
 /*   By: nboubeke <nboubeke@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 16:06:49 by nboubeke          #+#    #+#             */
-/*   Updated: 2026/03/21 16:06:51 by nboubeke         ###   ########.fr       */
+/*   Updated: 2026/03/23 19:18:19 by nboubeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+int	base_len(char *base);
+int	is_valid_base(char *base);
+int	index_in_base(char c, char *base);
+int	atoi_base(char *str, char *base);
 
+char	itoa(int nbr, char *base)
+{
+	char	*str;
+	int	i;
+	int	count;
+
+	count = 0;
+	if (nbr == 0)
+		return ('0');
+	while (nbr > 0)
+	{
+		nbr = nbr % base_len(base);
+		count++;
+	}
+	str = malloc(sizeof(char) * count);
+	i = 0;
+	while (i < count)
+	{
+		nbr = nbr % base_len(base);
+		str[i] = base[nbr];
+		i++;
+	}
+	return (*str);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int	atoi;
+	char	*final;
+
+	if (!(is_valid_base(base_from) || is_valid_base(base_to)))
+	{
+		return (0);
+	}
+	else
+	{
+		atoi = atoi_base(nbr, base_from);
+		return (itoa(atoi, base_to));
+	}
+}
+
+int main(void)
+{
+	char *nbr = "54";
+	char *base_from = "0123456789";
+	char *base_to = "0123456789ABCDEF";
+
+
+}
